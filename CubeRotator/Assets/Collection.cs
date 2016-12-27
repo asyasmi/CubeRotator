16ddb1b8ed47b21e74a6994a4987884d8c1b601c
asyasmi <asya.smi@gmail.com>
2016-12-27 18:24:10 +0200

    initial commit

---
+++ CubeRotator/Assets/Collection.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Collection : MonoBehaviour {
+
+	private Renderer [] CubesRN; 
+	int i;
+	public Button colorChange;
+
+	// Use this for initialization
+	void Start () {
+		colorChange = FindObjectOfType <Button> ();
+		CubesRN = FindObjectsOfType (typeof(Renderer)) as Renderer[];
+		i = 0;
+		colorChange.onClick.AddListener (TaskOnClick);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Color randColor = new Color (Random.Range(0f, 255f), Random.Range(0f, 255f), Random.Range(0f, 255f), 1f);
+	}
+
+	void ChangeColor (){
+		Color randColor = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+		foreach (Renderer CubeRN in CubesRN) {
+			CubeRN.material.color = randColor;
+			i++;
+			Debug.Log (i + ": Color was changed to " + randColor.ToString());
+		}
+	}
+
+	void TaskOnClick(){
+		ChangeColor ();
+	}
+
+}
---
+++ CubeRotator/Assets/CubesRotator.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CubesRotator : MonoBehaviour {
+
+	private Rigidbody[] CubesRB;
+	bool r, l, u, d; 
+	public Slider speedSlider;
+	private GameObject largeCube; 
+
+	// Use this for initialization
+	void Start () {
+		largeCube = GameObject.Find ("Cubes"); 
+		CubesRB = FindObjectsOfType (typeof(Rigidbody)) as Rigidbody[];
+		foreach (Rigidbody CubeRB in CubesRB) {
+			MyRotation (new Vector3 (0, 45, 0) * Time.smoothDeltaTime);
+		}
+		speedSlider = FindObjectOfType <Slider>(); 
+		speedSlider.onValueChanged.AddListener (delegate {ValueChangeCheck ();});
+		speedSlider.value = 1;
+
+	}
+
+	void ValueChangeCheck (){
+		Debug.Log (speedSlider.value);	
+	}
+
+
+	void MyRotation (Vector3 vector3) {
+		foreach (Rigidbody CubeRB in CubesRB) {
+			CubeRB.transform.Rotate (vector3);
+		}
+		largeCube.transform.Rotate (vector3); 
+	}
+	// Update is called once per frame
+
+	void Update () {
+		// Horizontal
+		if (Input.GetKey ("left")) {	
+			u = d = r = false;
+			l = true;	
+		} 
+		if (Input.GetKey ("right")){
+			u = d = l = false;
+			r = true; 
+		}  
+		//Vertical
+		if (Input.GetKey ("up")) {
+			l = d = r = false;
+			u = true;
+		}  
+		if (Input.GetKey ("down")) {
+			l = u = r = false;
+			d = true;
+		}
+		
+		if (l) foreach (Rigidbody CubeRB in CubesRB) {
+				MyRotation (new Vector3 (0, Time.smoothDeltaTime, 0) * speedSlider.value);
+			}
+		if (r) foreach (Rigidbody CubeRB in CubesRB) {
+				MyRotation (new Vector3 (0, -Time.smoothDeltaTime, 0) * speedSlider.value);
+			}
+		if (u) foreach (Rigidbody CubeRB in CubesRB) {
+				MyRotation (new Vector3 (Time.smoothDeltaTime, 0, 0) * speedSlider.value);
+			}
+		if (d) foreach (Rigidbody CubeRB in CubesRB) {
+				MyRotation (new Vector3 (-Time.smoothDeltaTime, 0, 0) * speedSlider.value);
+			}
+				
+	}
+}
